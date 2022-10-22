05bb44b069f2ec4adfc88f318a23aa780e09f00b
Hideki NIHOMMATSU <tech.hidewagon2000@gmail.com>
2022-10-23 02:44:19 +0900

    Added 3 Activity File

---
+++ Application/app/src/main/java/com/norihiro/myapplication/InputActivity.kt
@@ -0,0 +1,11 @@
+package com.norihiro.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
